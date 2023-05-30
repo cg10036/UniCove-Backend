88e402f19163ef8d8d80ac6ce0ae6de04d8ef6a4
cg10036 <cg10036@gmail.com>
2023-05-30 23:03:11 +0900

    fix: naver map api

--- src/util/navermap.js
+++ src/util/navermap.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 let page_uid = "",
   NNB = "";
 const userAgent =
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
 
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -50,8 +50,22 @@ const getSearchResult = async (type, query, options = {}, retry = 5) => {
       )}`,
       {
         headers: {
-          "user-agent": userAgent,
+          accept: "application/json, text/plain, */*",
+          "accept-encoding": "gzip, deflate, br",
+          "accept-language": "ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4",
+          "cache-control": "no-cache",
+          "content-type": "application/json",
           cookie: [page_uid, NNB].join("; "),
+          expires: "Sat, 01 Jan 2000 00:00:00 GMT",
+          pragma: "no-cache",
+          referer: "https://map.naver.com/",
+          "sec-ch-ua": `"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"`,
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": `"macOS"`,
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "user-agent": userAgent,
         },
       }
     );
